9f36e86173f987d1fc0ba2e17442e18bf20eaf67
shystruk <wystruk@ukr.net>
2019-11-29 15:52:18 -0800

    Fix CORS

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 8000;
 
-require('./routes')(app);
-
 app.use(cors());
+
+require('./routes')(app);
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
