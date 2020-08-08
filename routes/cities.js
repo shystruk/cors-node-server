9ed26b857c338c9e9071695dc8a058abace83dd2
shystruk <v.stokol@gmail.com>
2020-08-08 16:22:08 -0700

    connect to mongodb, editorconfig

--- routes/cities.js
+++ routes/cities.js
@@ -3,13 +3,13 @@
 const CONSTANTS = require('../constants');
 
 module.exports = function(app) {
-    app.get('/cities/:language', (req, res) => {
-        const language = req.params.language;
+	app.get('/cities/:language', (req, res) => {
+		const language = req.params.language;
 
-        if (language === 'en') {
-            res.status(200).json(CONSTANTS.CITIES_EN);
-        } else {
-            res.status(204).send({ message: 'No Content' });
-        }
-    });
+		if (language === 'en') {
+			res.status(200).json(CONSTANTS.CITIES_EN);
+		} else {
+			res.status(204).send({ message: 'No Content' });
+		}
+	});
 };
--- routes/cossacks.js
+++ routes/cossacks.js
@@ -3,13 +3,13 @@
 const CONSTANTS = require('../constants');
 
 module.exports = function(app) {
-    app.get('/cossacks/:language', (req, res) => {
-        const language = req.params.language;
+	app.get('/cossacks/:language', (req, res) => {
+		const language = req.params.language;
 
-        if (language === 'en') {
-            res.status(200).json(CONSTANTS.COSSACKS_EN);
-        } else {
-            res.status(204).send({ message: 'No Content' });
-        }
-    });
+		if (language === 'en') {
+			res.status(200).json(CONSTANTS.COSSACKS_EN);
+		} else {
+			res.status(204).send({ message: 'No Content' });
+		}
+	});
 };
